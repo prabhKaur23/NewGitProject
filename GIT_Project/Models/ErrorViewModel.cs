12d252dc628572c20e67964c4bc2ea9f17c0b6cb
prabhKaur23 <jot23990@gmail.com>
2023-03-03 19:08:11 +0530

    initial Commit

---
+++ GIT_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GIT_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
